a204a7c4a5a9f4e1dcd0f74ed8025b4f2cc23645
youmm <youmm@tongdaohui.com>
2017-08-24 18:33:25 +0800

    8.24六点 阮 进入debug环境测试

--- Weitianshi/app.js
+++ Weitianshi/app.js
@@ -820,7 +820,7 @@ App({
         error: 0,
         // url: "https://wx.weitianshi.cn",
         // url_common: "https://www.weitianshi.cn"
-        url: "https://wx.dev.weitianshi.cn",
-        url_common: "https://wx.dev.weitianshi.cn"
+        url: "https://wx.debug.weitianshi.cn",
+        url_common: "https://wx.debug.weitianshi.cn"
     },
 });
