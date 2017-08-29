d2b320b3430d7764f81d754b8884027d35681cf2
youmm <youmm@tongdaohui.com>
2017-08-29 16:07:49 +0800

    8.29 下午 阮
    1.开发加入战队页面

--- Weitianshi/pages/contactsActivty/joinWarband/joinWarband.js
+++ Weitianshi/pages/contactsActivty/joinWarband/joinWarband.js
@@ -7,6 +7,7 @@ Page({
         joinedWarband: [],
         warband: [],
         page_end: false,
+        timer:'',
     },
     onLoad(options) {
         let that = this;
@@ -14,8 +15,24 @@ Page({
         that.getInfo('', 1)
     },
     onShow() { },
-    //选中noChecked
-    noChecked(e) {
+    //搜索战队
+    searchSth(e){
+        let that=this;
+        let timer=this.data.timer;
+        // console.log(str)
+        //防止多次请求进行延时处理
+        if(timer){
+            clearTimeout(timer)
+        }
+        timer=setTimeout(x=>{
+            this.getInfo(str,1)
+        },1000)
+        this.setData({
+            timer:timer
+        })
+    },
+    //所有战队勾选框
+    checkbox1(e) {
         let team=e.currentTarget.dataset.team;
         let index=e.currentTarget.dataset.index;
         let joinedWarband=this.data.joinedWarband;
@@ -36,13 +53,24 @@ Page({
             joinedWarband:joinedWarband
         })
     },
-    //选中checked
-    checked(e) {
-        console.log(e)
+    //加入战队勾选框
+    checkbox2(e){
+        let teams=e.currentTarget.dataset.team;
+        let joinedWarband=this.data.joinedWarband;
+        joinedWarband.forEach((x,index)=>{
+            console.log(x.team_id,teams.team_id)
+            if(x.team_id===teams.team_id){
+                joinedWarband.splice(index,1)
+            }
+        })
+        this.setData({
+            joinedWarband:joinedWarband
+        })
     },
     //获取战队信息
     getInfo(search, page) {
         let that = this;
+        let joinedWarband=that.data.joinedWarband;
         wx.request({
             url: url + '/api/team/search',
             data: {
@@ -53,12 +81,23 @@ Page({
             success(res) {
                 if (res.data.status_code === 2000000) {
                     let thisData = res.data.data;
+                    let teams=thisData.teams;
                     console.log('teams',thisData.teams)
-                    thisData.teams.forEach(x=>{
+                    teams.forEach(x=>{
                         x.check=false;
                     })
+                    //给已经加入的战队重新挂上check=true属性
+                    if(joinedWarband && teams && joinedWarband.length!=0){
+                        teams.forEach(x=>{
+                            joinedWarband.forEach(y => {
+                                if (x.team_id === y.team_id) {
+                                    x.check = true;
+                                }
+                            })
+                        })
+                    }
                     that.setData({
-                        warband: thisData.teams,
+                        warband: teams,
                         page_end: thisData.page_end
                     })
                 } else {
