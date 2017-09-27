a8128f6c65ac75ad18e38214a9badcf7ad77ac97
lovefruit1314 <521fruit.love@163.com>
2017-09-27 15:46:34 +0800

    霍春阳
    1.约谈项目页面,静态页面
    2.加载更多

--- Weitianshi/pages/message/contactProject/projectList/projectList.js
+++ Weitianshi/pages/message/contactProject/projectList/projectList.js
@@ -38,5 +38,61 @@ Page({
         })
       }
     })
+
+    //向后台发送信息取消红点
+    wx.request({
+      url: url_common + '/api/project/metViewed',
+      data: {
+        user_id: user_id,
+        type_id: 9
+      },
+      method: "POST",
+      success:function(res){
+        console.log(res)
+      }
+    })
+    this.setData({
+      requestCheck: true,
+      currentPage: 1,
+      page_end: false
+    })
+  },
+  loadMore: function () {
+    var that = this;
+    let user_id = wx.getStorageSync('user_id');//获取我的user_id
+    let project_id = this.data.project_id;
+    var currentPage = this.data.currentPage;
+    let metList = this.data.metList;
+    let list = this.data.list;
+    var request = {
+      url: url_common + '/api/project/myMet',
+      data: {
+        user_id : user_id,
+        type_id: 9,
+        project_id: project_id,
+        page: currentPage
+      },
+    }
+    //调用通用加载函数
+    app.loadMore2(that, request, res => {
+      let rank = res.data.data.messages;
+      let page_end = res.data.data.page_end;
+      if (rank) {
+        let newRank_list = metList.concat(rank)
+        that.setData({
+          metList: newRank_list,
+          page_end: page_end,
+          requestCheck: true
+        })
+      }
+    })
+  },
+  //点击跳转到用户详情
+  personDetail: function (e) {
+    var id = e.currentTarget.dataset.project;
+    app.console(id)
+    wx.navigateTo({
+      url: '/pages/userDetail/networkDetail/networkDetail?id=' + id,
+    })
   },
 })
