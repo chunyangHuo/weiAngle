7d7949815a97ffe198f93275ae0c9a525e2de67c
morganfly <786485758@qq.com>
2017-11-24 11:30:32 +0800

    11.24 阮 11.29
    1.合并bindPhone和personInfo页面

--- Weitianshi/pages/register/personInfo/personInfo.js
+++ Weitianshi/pages/register/personInfo/personInfo.js
@@ -3,9 +3,9 @@ var url = app.globalData.url;
 var url_common = app.globalData.url_common;
 Page({
   data: {
-    name: "",
-    telephone: null,
-    checkCode: "",
+    name: '',
+    telephone: '',
+    checkCode: '',
     result: "0",//手机号码验证是否正确
     error: "0",
     error_text: '',
@@ -13,7 +13,8 @@ Page({
     time: "0",//获取验证码按钮是否可点
     loading: "0",//加载动画控制
     getCode: "获取验证码",
-    endTime: 60//多少秒后验证码得发
+    endTime: 60,//多少秒后验证码得发
+    isShow: 0,//显示personInfo页面还是bindPhone页面
   },
   onLoad: function (options) {
     let type = options.type;
@@ -25,15 +26,27 @@ Page({
     }
   },
   onShow: function () {
+    var that = this;
     //获取code,服务于微信授权绑定
+    this.getCode();
+    this.clearTime();
+  },
+  //获取code存入data中,服务于微信授权绑定(辅助函数)
+  getCode() {
+    let that = this;
     wx.login({
       success(res) {
         if (res.code) {
-          that.data.code = res.code;
+          that.setData({
+            code: res.code
+          })
         }
       }
     })
-    var that = this;
+  },
+  // 清除记时器(辅助函数)
+  clearTime() {
+    let that = this;
     if (this.data._time) {
       that.setData({
         time: "1"
@@ -44,8 +57,6 @@ Page({
         time: "0"
       })
     }
-  },
-  onHide: function () {
   },
   //姓名
   stripscript: function (e) {
@@ -135,7 +146,7 @@ Page({
   onPullDownRefresh: function () {
     wx.stopPullDownRefresh()
   },
-  //点击跳转
+  //personInfo点击跳转
   nextPage: function () {
     let that = this;
     let type = this.data.type;
@@ -187,6 +198,68 @@ Page({
       }
     })
   },
+  //其他手机绑定
+  goBack() {
+    this.getCode();
+    this.clearTime();
+    this.setData({
+      isShow: 0,
+      name:'',
+      telephone:'',
+      checkCode:'',
+    })
+  },
+  //bindPhone点击跳转
+  nextPage1() {
+    let that = this;
+    let type = this.data.type;
+    wx.login({
+      success: function (res) {
+        var name = that.data.name;
+        var telephone = that.data.telephone;
+        var result = that.data.result;
+        var error = that.data.error;
+        var error_text = that.data.error_text;
+        var open_session = wx.getStorageSync('open_session')
+        console.log(open_session)
+        if (!name) {
+          app.errorHide(that, '姓名不能为空', 3000)
+        } else if (!telephone) {
+          app.errorHide(that, '手机号码不能为空', 3000)
+        } else if (telephone == 'undefined') {
+          app.errorHide(that, '获取手机号码失败,请重试', 3000)
+        } else {
+          wx.request({
+            url: url_common + '/api/user/bindUser',
+            data: {
+              user_real_name: name,
+              user_mobile: telephone,
+              open_session: open_session,
+              oauth: "wx_oauth"
+            },
+            method: 'POST',
+            success: function (res) {
+              var user_career = res.data.user_career;
+              var user_company = res.data.user_company;
+              var uer_email = res.data.user_email;
+              if (res.data.status_code == 2000000) {
+                wx.setStorageSync('user_id', res.data.user_id);
+                app.globalData.user_id = res.data.user_id;
+                if (type) {
+                  app.href('/pages/register/companyInfo/companyInfo?user_career=' + user_career + "&&user_company=" + user_company + "&&uer_email=" + uer_email + '&&type=' + type, )
+
+                } else {
+                  app.href('/pages/register/companyInfo/companyInfo?user_career=' + user_career + "&&user_company=" + user_company + "&&uer_email=" + uer_email, )
+                }
+              } else {
+                app.errorHide(that, res.data.error_msg, 3000)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
   //微信授权绑定
   getPhoneNumber(e) {
     console.log(e)
@@ -204,10 +277,16 @@ Page({
       },
       method: "POST",
       success(res) {
-        console.log(res)
-        let telephone = res.data.user_mobile;
-        app.href('/pages/register/bindPhone/bindPhone?name=' + name + '&&telephone=' + telephone)
-
+        if (res.data.status_code == 2000000) {
+          let telephone = res.data.user_mobile;
+          that.setData({
+            telephone: telephone,
+            isShow: 1
+          })
+          // app.href('/pages/register/bindPhone/bindPhone?name=' + name + '&&telephone=' + telephone)
+        } else {
+          that.getCode();
+        }
       }
     })
   }
