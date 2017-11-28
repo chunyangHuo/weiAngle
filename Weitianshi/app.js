a629d076e8b609f04aa2737be600059d5de545cd
morganfly <786485758@qq.com>
2017-11-28 14:27:56 +0800

    添加了tran_缓存

--- Weitianshi/app.js
+++ Weitianshi/app.js
@@ -65,6 +65,11 @@ App({
         wx.setStorageSync("stage", thisData.stage)
         wx.setStorageSync('area', thisData.area)
         wx.setStorageSync('hotCity', thisData.hotCity)
+        wx.setStorageSync("tran_industry",'')
+        wx.setStorageSync("tran_scale", '')
+        wx.setStorageSync("tran_stage", '')
+        wx.setStorageSync('tran_area', '')
+        wx.setStorageSync('tran_hotCity', '')
         console.log(thisData)
       },
     })
