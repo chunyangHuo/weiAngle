302cd17083cf3811ae6799867c27503102c6965d
morganfly <786485758@qq.com>
2018-01-12 14:05:33 +0800

    1.12 阮 14:04
    1.活动分包,signCheckIndentity页面开发

--- Weitianshi/activitySignIn/pages/signCheckIndentity/signCheckIndentity.js
+++ Weitianshi/activitySignIn/pages/signCheckIndentity/signCheckIndentity.js
@@ -1,66 +1,15 @@
 // pages/activity_detailPreview/activity_detailPreview.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    demo: [
+      {
+        name: '名称',
+        value: '第一届中国微天使节和第二十届中国BD岁末嘉年华（杭州场）开始报名！'
+      },
+      {
+        name: '主办方',
+        value: '浙江省杭州市西湖区文一西路588号中节能西溪首座B3-51信用卡-1楼'
+      }
+    ]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
