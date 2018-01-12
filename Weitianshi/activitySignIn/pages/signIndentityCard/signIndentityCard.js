294adf54a1b3fd18e9769111c99b40b8bb0e2abc
morganfly <786485758@qq.com>
2018-01-12 17:34:24 +0800

    1.12 阮 17:34
    1.开发活动1.0

--- Weitianshi/activitySignIn/pages/signIndentityCard/signIndentityCard.js
+++ Weitianshi/activitySignIn/pages/signIndentityCard/signIndentityCard.js
@@ -3,10 +3,6 @@ var app = getApp();
 var url = app.globalData.url;
 var url_common = app.globalData.url_common;
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
     userInfo: {
       userName: "这里是姓名",
@@ -24,60 +20,21 @@ Page({
       content: '浙江省杭州市西湖区文一西路588号中节能西溪首座2微天使乐投平台、智云社微天使乐投平台、智云社微...'
     }
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    console.log(this.data);
+  onLoad(){
+    app.loginPage(user_id => {
+      this.getActivityDetail();
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getActivityDetail(){
+    app.httpPost({
+      url: url_common +'/api/activity/getActivityEntrance',
+      data: {
+        activity_id: 1,
+        apply_id: "",
+        open_session: app.globalData.open_session
+      }
+    },this).then(res => {
+      console.log(res)
+    })
   }
 })
