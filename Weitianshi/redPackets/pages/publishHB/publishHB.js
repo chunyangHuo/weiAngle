0efb7cebf06c67bf8a7ad66d06a6182213b6e451
lovefruit1314 <521fruit.love@163.com>
2018-01-29 18:09:08 +0800

    1-29-18:00-霍春阳
    ui样式

--- Weitianshi/redPackets/pages/publishHB/publishHB.js
+++ Weitianshi/redPackets/pages/publishHB/publishHB.js
@@ -1,16 +1,36 @@
- let app = getApp();
- let RP = require('../../../utils/model/redPackets.js')
- var url = app.globalData.url;
- var url_common = app.globalData.url_common;
+let app = getApp();
+let RP = require('../../../utils/model/redPackets.js')
+var url = app.globalData.url;
+var url_common = app.globalData.url_common;
 Page({
   data: {
-  
+    imgUrls: app.globalData.picUrl.bg_hongbao,
+    imgUrls2: app.globalData.picUrl.shengcheng
   },
-  onLoad(){
+  onLoad() {
     let rp = new RP.redPackets();
     console.log(rp)
     app.loginPage(user_id => {
-      
+
     })
-  }
+  },
+  formSubmit(e) {     
+    this.setData({
+      formId: e.detail.formId
+    })
+  },
+
+  redBagName(e) {
+    let bagName = e.detail.value;
+  },
+  redBagMoney(e) {
+    let bagMoney = e.detail.value;
+  },
+  redBagNum(e) {
+    let bagNum = e.detail.value;
+  },
+  createRedBag() {
+    console.log(55)
+  },
+
 })
