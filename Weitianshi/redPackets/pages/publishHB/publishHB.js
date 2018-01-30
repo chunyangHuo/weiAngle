dce0ffeaa882b227b1f5ca2f6d7d6d2bde803c6c
lovefruit1314 <521fruit.love@163.com>
2018-01-30 15:29:58 +0800

    1-30-霍春阳-15:30
    调节样式

--- Weitianshi/redPackets/pages/publishHB/publishHB.js
+++ Weitianshi/redPackets/pages/publishHB/publishHB.js
@@ -7,10 +7,12 @@ Page({
   data: {
     imgUrls: app.globalData.picUrl.bg_hongbao,
     imgUrls2: app.globalData.picUrl.shengcheng,
-    nonet: true
+    nonet: true,
+    number: 0,
+    money: 0,
+    title : ""
   },
   onLoad() {
-    console.log(rp)
     app.loginPage(user_id => {
 
     })
@@ -18,32 +20,27 @@ Page({
 
   redBagName(e) {
     let bagName = e.detail.value;
-    console.log(bagName.length)
     this.setData({
-      title : bagName
+      title: bagName
     })
   },
   redBagMoney(e) {
     let bagMoney = e.detail.value;
-    setTimeout(x => {
-      this.getAll(bagMoney);
-    }, 1000);
-    // if(!moneyReg.test(bagMoney)){
-    //   // app.errorHide(that, "大于1元", 1500);
-    // }else{
-    //   this.setData({
-    //     money: bagMoney
-    //   })
-    // }
-   
+    this.setData({
+      money: bagMoney
+    })
+    // clearTimeout(A)
+    // let A = setTimeout(x => {
+    //   this.getAll(bagMoney);
+    // }, 1000);
   },
   redBagNum(e) {
-    let  that = this;
+    let that = this;
     let bagNum = e.detail.value;
     let regNum = /^[+]{0,1}(\d+)$/;
-    if (!regNum.test(bagNum)){
+    if (!regNum.test(bagNum)) {
       app.errorHide(that, "需要是整数哦", 1500);
-    }else{
+    } else {
       this.setData({
         number: bagNum
       })
@@ -51,16 +48,33 @@ Page({
   },
   // 发布红包
   createRedBag(e) {
+    let  that = this;
     let user_id = wx.getStorageSync('user_id');
     let number = this.data.number;
     let money = this.data.money;
     let title = this.data.title;
-    app.formIdSubmit(e);     
-    rp.publishHB.call(this,user_id,number,money,title)
+    app.formIdSubmit(e);
+    if (title == ''){
+      app.errorHide(that, "请填写标题", 1500);
+    } else if (money == 0){
+      app.errorHide(that, "请输入金额", 1500);
+    }else if(number == 0){
+      app.errorHide(that, "请输入红包个数", 1500);
+    }else(
+      rp.publishHB.call(this, user_id, number, money, title)
+    )
+    
   },
   //获取全部文字
-  getAll(bagMoney){
-    let moneyReg = /^[1-9]+(.[0-9]{1,2})?$/
-    console.log(moneyReg.test(bagMoney))
+  getAll(bagMoney) {
+    let  that = this;
+    let moneyReg = /^[1-9]+(.[0-9]{0,2})?$/
+    if (!moneyReg.test(bagMoney)) {
+      app.errorHide(that, "需要大于1元", 1500);
+    } else {
+      this.setData({
+        money: bagMoney
+      })
+    }
   }
 })
