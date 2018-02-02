f12ff2e9cdf798d394b7552b82513e85350e4b5f
lovefruit1314 <521fruit.love@163.com>
2018-02-02 15:53:56 +0800

    2-2-霍春阳16:00
    提交代码

--- Weitianshi/redPackets/pages/publishHB/publishHB.js
+++ Weitianshi/redPackets/pages/publishHB/publishHB.js
@@ -56,6 +56,7 @@ Page({
     let number = this.data.number;
     let money = this.data.money;
     let title = this.data.title;
+    console.log(money)
     app.formIdSubmit(e);
     if (money == 0) {
       app.errorHide(that, "请输入金额", 1500);
@@ -63,24 +64,30 @@ Page({
       app.errorHide(that, "请输入红包个数", 1500);
     } else if (money / number < 1) {
       app.errorHide(that, "红包领取金额不能小于1", 1500);
-    } else if (money / number >= 1){
+    } else if (money / number >= 1) {
       rp.publishHB.call(this, user_id, number, money, title)
     }
-  
+
   },
   //获取全部文字
   getAll(bagMoney) {
     let that = this;
-    let moneyReg = /^[1-9]+(.[0-9]{0,2})?$/
-    if (!moneyReg.test(bagMoney)) {
-      app.errorHide(that, "需要大于1元", 1500);
+    let moneyReg = /^[1-9]+(.[0-9]{0,2})?$/;
+    if (moneyReg.test(bagMoney)) {
+      if (bagMoney > 2018) {
+        app.errorHide(that, "不能大于2018元", 1500);
+      } else if (1 <= bagMoney <= 2018) {
+        if (!moneyReg.test(bagMoney)) {
+          app.errorHide(that, "红包金额最多只能是两位小数", 1500);
+        } else {
+          this.setData({
+            money: bagMoney
+          })
+        }
+      }
     } else {
-      if (bagMoney >= 2018) {
-        app.errorHide(that, "不能大于2018", 1500);
-      } else {
-        this.setData({
-          money: bagMoney
-        })
+      if (bagMoney < 1) {
+        app.errorHide(that, "红包金额不能小于1元", 1500);
       }
     }
   }
